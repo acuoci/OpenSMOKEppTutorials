bc791c593f6da2e22d8cd6c7ebf32679b782a90a
Alberto Cuoci <alberto.cuoci@polimi.it>
2016-04-09 11:37:10 +0200

    Fixed bug in batch reactor tutorials

--- examples/OpenSMOKE_BatchReactor/06a-multiple-simulations/Run.bat
+++
@@ -1,4 +0,0 @@
-%OPENSMOKEPP_EXE_FOLDER%\OpenSMOKE_BatchReactor.exe --dictionary BatchReactor-CVODE
-%OPENSMOKEPP_EXE_FOLDER%\OpenSMOKE_BatchReactor.exe --dictionary BatchReactor-DVODE
-%OPENSMOKEPP_EXE_FOLDER%\OpenSMOKE_BatchReactor.exe --dictionary BatchReactor-DASPK
-pause
