beadd49f64a225c5f0cb478837d0a85f9c8992bd
Alberto Cuoci <alberto.cuoci@polimi.it>
2016-09-30 01:18:00 +0200

    Fixed uncorrect names in microgravity droplet examples

--- examples/OpenSMOKEpp_MicrogravityDroplet/01-heptane-evaporation/Run.bat
+++
@@ -1,2 +0,0 @@
-"C:\Development\OpenSMOKEpp\OpenSMOKEppSolvers\exe\windows\msvs2015\x64\Release\OpenSMOKEpp_MicrogravityDroplet.exe" --input input.dic
-pause
---
+++ examples/OpenSMOKEpp_MicrogravityDroplet/02-heptane-combustion/Run.droplet.bat
@@ -0,0 +1,2 @@
+"%OPENSMOKEPP_EXE_FOLDER%\OpenSMOKEpp_MicrogravityDroplet.exe" --input input.droplet.dic
+pause
