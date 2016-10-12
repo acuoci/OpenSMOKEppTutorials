b2b228afff7829009ac07b3cd187d54ea9678e16
Alberto Cuoci <alberto.cuoci@polimi.it>
2016-10-12 13:49:37 +0200

    Added updated examples fro microgravity droplet

--- examples/OpenSMOKEpp_MicrogravityDroplet/01-heptane-evaporation/Run.preprocessing.bat
+++ examples/OpenSMOKEpp_MicrogravityDroplet/01-heptane-evaporation/Run.preprocessing.bat
@@ -1,2 +1,2 @@
-"%OPENSMOKEPP_EXE_FOLDER%\OpenSMOKEpp_CHEMKIN_PreProcessor.exe" --input input.preprocessing.dic
+"%C:\Development\OpenSMOKEpp\OpenSMOKEppSolvers\exe\windows\msvs2015\x64\Release\OpenSMOKEpp_CHEMKIN_PreProcessor.exe" --input input.preprocessing.dic
 pause
