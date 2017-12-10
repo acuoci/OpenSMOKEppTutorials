2730e2a2fe4d54c313c7bcdafdcdbd18c3f2bb42
Alberto Cuoci <alberto.cuoci@polimi.it>
2017-12-10 22:50:45 +0100

    Updated microgravity droplet examples

--- examples/OpenSMOKEpp_MicrogravityDroplet/02-mixture-thermogravity/Run.sh
+++ examples/OpenSMOKEpp_MicrogravityDroplet/02-mixture-thermogravity/Run.sh
@@ -1,1 +1,1 @@
-/home/chimica2/stagni/Development/OpenSMOKE_MicrogravityDroplet/exe/linux/gnu64-serial-openblas/MicrogravityDroplet.sh
+MicrogravityDroplet.sh --input input.thermo.dic --type thermogravimetry
