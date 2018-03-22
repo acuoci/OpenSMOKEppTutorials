d6ef0543dda14d62e03f5a193d1a3b70cfdd3110
Alberto Cuoci <alberto.cuoci@polimi.it>
2018-03-22 17:10:36 +0100

    Added example about abstraction reactions

---
+++ examples/OpenSMOKEpp_CHEMKIN_PreProcessor/12-abstractions/Run.bat
@@ -0,0 +1,3 @@
+"%OPENSMOKEPP_EXE_FOLDER%\OpenSMOKEpp_CHEMKIN_PreProcessor.exe" --input input.abstractions.dic
+"%OPENSMOKEPP_EXE_FOLDER%\OpenSMOKEpp_CHEMKIN_PreProcessor.exe" --input input.dic
+pause
