4c280a04dbc952f73b204eaf90765ede3f230781
Alocaspi <86231852+Alocaspi@users.noreply.github.com>
2023-03-28 11:51:32 +0200

    Fixed exe path TG
    Fixed with proper environment variable the run.thermo.bat for HDPE bubble distribution

--- examples/OpenSMOKEpp_MicrogravityDroplet/03-hdpe-thermogravimetry_bubbles/Run.thermo.bat
+++ examples/OpenSMOKEpp_MicrogravityDroplet/03-hdpe-thermogravimetry_bubbles/Run.thermo.bat
@@ -1,2 +1,2 @@
-"%LIQUID_EXE%\OpenSMOKEpp_MicrogravityDroplet.exe" --input input.thermo.dic --type thermogravimetry
+"%OPENSMOKEPP_EXE_FOLDER%\OpenSMOKEpp_MicrogravityDroplet.exe" --input input.thermo.dic --type thermogravimetry
 pause
