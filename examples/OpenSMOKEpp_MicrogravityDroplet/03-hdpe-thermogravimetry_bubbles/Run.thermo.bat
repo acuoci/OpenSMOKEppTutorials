f8ff6367b5b95df1129b98acb41287f78fff3d69
Alocaspi <86231852+Alocaspi@users.noreply.github.com>
2023-03-28 11:48:02 +0200

    Thermogravimetry polymers tutorials
    Updated the microgravity droplets with tutorials for the thermogravimetry of plastics:
    - Introduced HDPE kinetic mechanism and examples with different evaporation models (bubble distribution, boiling temperatures, global transport coefficients)
    - tutorials for liquid-phase ropa
    - liquid-phase properties of HDPE species (fake apart for Tc, Pc and Tnb!)

---
+++ examples/OpenSMOKEpp_MicrogravityDroplet/03-hdpe-thermogravimetry_bubbles/Run.thermo.bat
@@ -0,0 +1,2 @@
+"%LIQUID_EXE%\OpenSMOKEpp_MicrogravityDroplet.exe" --input input.thermo.dic --type thermogravimetry
+pause
---
+++ examples/OpenSMOKEpp_MicrogravityDroplet/03-hdpe-thermogravimetry_globalcoefficient/Run.thermo.bat
@@ -0,0 +1,2 @@
+"%OPENSMOKEPP_EXE_FOLDER%\OpenSMOKEpp_MicrogravityDroplet.exe" --input input.thermo.dic --type thermogravimetry
+pause
