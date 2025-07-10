d0855f050e07e575b470bce4e3a2e93efb2b4dc9
Alocaspi <86231852+Alocaspi@users.noreply.github.com>
2025-07-10 14:47:03 +0200

    Microgravity - Compulsory Density keyword and Thermogravimetry pre-processing
    Modified all microgravity examples, to include the compulsory keyword Density in LiquidMixture dictionary. Also rearranged HDPE examples such that the preprocessing of the kinetic model occurs in the same folder as the simulation

---
+++ examples/OpenSMOKEpp_MicrogravityDroplet/03d-hdpe-thermogravimetry_evaporationkinetics/Run.preprocessing.bat
@@ -0,0 +1,2 @@
+"%OPENSMOKEPP_EXE_FOLDER%\OpenSMOKEpp_CHEMKIN_PreProcessor.exe" --input ../../OpenSMOKEpp_CHEMKIN_PreProcessor/13b-hdpe_evaporationkinetics/input.dic
+pause
---
+++ examples/OpenSMOKEpp_MicrogravityDroplet/03e-hdpe-thermogravimetry_ropa/Run.preprocessing.bat
@@ -0,0 +1,2 @@
+"%OPENSMOKEPP_EXE_FOLDER%\OpenSMOKEpp_CHEMKIN_PreProcessor.exe" --input ../../OpenSMOKEpp_CHEMKIN_PreProcessor/13a-hdpe/input.dic
+pause
